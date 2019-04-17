fdba37c6b754253df3988b732ae3385a8613ee07
Aaron <aaron.cruz@epn.edu.ec>
2019-04-16 19:30:59 -0500

    Ejercicio2-Funcion

--- ejercicio2/index.php
+++ ejercicio2/index.php
@@ -2,6 +2,25 @@
 /**
  * Problema propuesto:
  *
- * Implementar una función que muestre un título centrado en pantalla, y la llamaremos posteriormente dos veces.
+ * Implementar una función que muestre un título centrado en pantalla, 
+ * y la llamaremos posteriormente dos veces.
 */
+
+function titulo($title){
+    echo"<h1 style=\"text-align:center\">";
+    echo $title;
+    echo"</h1>";
+}
 ?>
+
+<!DOCTYPE>
+<html>
+<head>
+</head>
+<body>
+    <?php
+    titulo("Hola Mundo");
+    titulo("Esto Es PHP");
+    ?>
+</body>
+</html>
