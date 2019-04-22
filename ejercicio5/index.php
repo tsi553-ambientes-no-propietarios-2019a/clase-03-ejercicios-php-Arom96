c29bf1fbdccd8f3ffec089384f6bade8cdd109aa
Aaron <aaron.cruz@epn.edu.ec>
2019-04-22 18:52:09 -0500

    Ejercicio5-NúmeroAleatorio

--- ejercicio5/index.php
+++ ejercicio5/index.php
@@ -1,15 +1,27 @@
 <?php
 
+
 /**
- Sabiendo que la función rand nos retorna un valor aleatorio entre un rango de dos enteros:
-
-$num=rand(1,100);
-En la variable $num se almacena un valor entero que la computadora genera en forma aleatoria entre 1 y 100.
-
-Hacer un programa que lo muestre por pantalla al valor generado. Mostrar además si es menor o igual a 50 o si es mayor.
-
-Para imprimir el contenido de una variable también utilizamos el comando echo:
-
-echo $num;
+*Sabiendo que la función rand nos retorna un valor aleatorio 
+*entre un rango de dos enteros:
+*$num=rand(1,100);
+*En la variable $num se almacena un valor entero que la 
+*computadora genera en forma aleatoria entre 1 y 100.
+*Hacer un programa que lo muestre por pantalla al valor generado. 
+*Mostrar además si es menor o igual a 50 o si es mayor.
+*Para imprimir el contenido de una variable también utilizamos
+*el comando echo:
+*echo $num;
  */
+        
+$numero = rand(1,100);
+echo"<h1 style=\"text-align:center\">";
+echo $numero;
+echo"</h1>";
+echo "<br>";
+if($numero<=50){
+    echo"El número es menor a 50";
+    }elseif($numero>50){
+        echo "El número es mayor a 50";
+}
 ?>
